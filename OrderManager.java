f3727345c99b50ee467c78b485377737ad999929
BBorislavov <btborislavov@gmail.com>
2016-03-24 09:54:44 -0700

    Add files

---
+++ OrderManager.java
@@ -0,0 +1,15 @@
+
+public class OrderManager {
+
+	// NOT NECESSARY RIGHT NOW
+	// serialize/deserialize
+	private DeliveryRequest mDeliveryRequest;
+
+	public OrderManager(DeliveryRequest deliveryRequest) {
+		mDeliveryRequest = deliveryRequest;
+	}
+
+	// DroneManager droneManager = new DroneManager(5);
+	// droneManager.filfillOrder(mDeliveryRequest);
+
+}
---
+++ RequestManager.java
@@ -0,0 +1,28 @@
+public class RequestManager {
+
+	public void manageRequests(DroneManager dm, WarehouseManager warehouseManager) {
+
+		InputReader ir = new InputReader("/home/ubuntu/workspace/Week13Monday_2202/src/testDeliveryRequests");
+		String requestType = ir.getNextRequestType();
+
+		// System.out.println(warehouseManager.getAvailableProducts());
+		while (requestType != null) {
+			if (requestType.equals("supply")) {
+				SupplyRequest currentSupplyRequest = ir.convertToSupplyRequest();
+				warehouseManager.addToAvailability(currentSupplyRequest);
+				// System.out.println(warehouseManager.getAvailableProducts());
+			} else if (requestType.equals("delivery")) {
+				DeliveryRequest currentDeliveryRequest = ir.convertToDeliveryRequest();
+				double DeliveryTimeTotal = dm.filfillOrder(currentDeliveryRequest);
+				if (DeliveryTimeTotal == -1) {
+					System.out.println("The drones could not make the delivery");
+				} else {
+					System.out.println("The time it took the drones to make the delivery was "
+							+ dm.filfillOrder(currentDeliveryRequest));
+					// this is just for testing
+				}
+			}
+			requestType = ir.getNextRequestType();
+		}
+	}
+}
